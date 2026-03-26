0eda552a3788da5a0df1b2e76ac0d984033d5a94
Kartikeya <kartikeyaku@gmail.com>
2026-03-26 05:05:58 -0700

    Revised skills section

--- website/app/components/Sections/Skills.tsx
+++ website/app/components/Sections/Skills.tsx
@@ -6,11 +6,14 @@ export default function Skills() {
       <div className="container">
         <h2>Skills</h2>
         <ul>
+          <li>HTML5, CSS3, Tailwind CSS</li>
           <li>JavaScript / TypeScript</li>
+          <li>Git Workflow (GitHub, GitLab, and Azure DevOps)</li>
           <li>React.js / Next.js</li>
-          <li>Django / Django REST Framework</li>
-          <li>Node.js, SQL (MySQL, PostgreSQL)</li>
-          <li>Agile, Git, Debugging, Communication</li>
+          <li>RESTful API Development</li>
+          <li>Django, Node.js, Sequelize ORM, Express, and Nest.js</li>
+          <li>NoSQL + SQL Databases (MongoDB, MySQL, PostgreSQL)</li>
+          <li>Agile, Scrum, Cross-Team Collaboration</li>
         </ul>
       </div>
     </section>
